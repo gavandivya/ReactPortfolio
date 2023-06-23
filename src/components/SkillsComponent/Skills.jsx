f719ca67f84ce860ad72f11084b665e11d671542
Divya R. Gavandi <divya190399@gmail.com>
2023-06-23 22:13:29 +0530

    feat:skills

--- src/components/SkillsComponent/Skills.jsx
+++ src/components/SkillsComponent/Skills.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Skills = () => {
-  return <div>Skills</div>;
+  return (
+    <div>
+      <h2 className="text-2xl font-bold uppercase mb-3">Skills</h2>
+    </div>
+  );
 };
 
 export default Skills;
