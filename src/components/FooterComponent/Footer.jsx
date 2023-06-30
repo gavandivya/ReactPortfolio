027c72452c4d7ef41bd91b24ab1c9903c9ab168d
gavandivya <divya190399@gmail.com>
2023-07-01 00:36:26 +0530

    fix:minor

--- src/components/FooterComponent/Footer.jsx
+++ src/components/FooterComponent/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => {
   return (
     <footer className="text-center bg-slate-400 p-4">
-      ©All right reserved. Designed with 💖 by Divya Gavandi
+      ©All right reserved. Designed with 💖 by Divya Gavandi.
     </footer>
   );
 };
