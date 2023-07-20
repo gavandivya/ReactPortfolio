a65fc29a0e662439d0b3447d456fe88f4c64ec3f
gavandivya <divya190399@gmail.com>
2023-07-20 18:55:20 +0530

    fix:Formatting

--- src/components/IntroComponent/Intro.jsx
+++ src/components/IntroComponent/Intro.jsx
@@ -10,7 +10,6 @@ import {
 import { SiHashnode } from "react-icons/si";
 import "./Intro.css";
 
-
 const Intro = () => {
   useEffect(() => {
     const typed = new Typed(".heading-typed", {
@@ -37,8 +36,7 @@ const Intro = () => {
   }, []);
 
   return (
-    <header className="white-text-container section-container"
-    >
+    <header className="white-text-container section-container">
       <div className="text-center">
         <h1 className="heading text-white">
           I am Divya Gavandi,
@@ -50,16 +48,10 @@ const Intro = () => {
           <a href="https://github.com/gavandivya">
             <AiFillGithub className="icons" />
           </a>
-          <a
-            href="https://www.linkedin.com/in/divya-gavandi/"
-            title="Linkedin"
-          >
+          <a href="https://www.linkedin.com/in/divya-gavandi/" title="Linkedin">
             <AiFillLinkedin className="icons" />
           </a>
-          <a
-            href="https://divya-gavandi19.hashnode.dev/"
-            title="Hashnode"
-          >
+          <a href="https://divya-gavandi19.hashnode.dev/" title="Hashnode">
             <SiHashnode className="icons" />
           </a>
           <a
@@ -68,34 +60,14 @@ const Intro = () => {
           >
             <AiFillYoutube className="icons" />
           </a>
-          <a
-            href="https://www.instagram.com/gavandivya/"
-            title="Instagram"
-          >
+          <a href="https://www.instagram.com/gavandivya/" title="Instagram">
             <AiFillInstagram className="icons" />
           </a>
         </div>
         <p className="glow">A girl who believes in GODFIDENCE</p>
       </div>
     </header>
-  ); 
+  );
 };
 
 export default Intro;
-
-/*  <!--/ Intro Skew Star /-->
-  <div id="home" class="intro route bg-image" style="background-image: url(img/intro-bg.jpg)">
-  <div class="overlay-itro"></div>
-  <div class="intro-content display-table">
-    <div class="table-cell">
-      <div class="container">
-        <!--<p class="display-6 color-d">Hello, world!</p>-->
-        <h1 class="intro-title mb-4">I am Morgan Freeman</h1>
-        <p class="intro-subtitle"><span class="text-slider-items">CEO DevFolio,Web Developer,Web Designer,Frontend Developer,Graphic Designer</span><strong class="text-slider"></strong></p>
-        <!-- <p class="pt-3"><a class="btn btn-primary btn js-scroll px-4" href="#about" role="button">Learn More</a></p> -->
-      </div>
-    </div>
-  </div>
-</div>
-<!--/ Intro Skew End /-->
-*/
--- src/components/SkillsComponent/Skills.jsx
+++ src/components/SkillsComponent/Skills.jsx
@@ -6,7 +6,7 @@ const Skills = () => {
     <div>
       <h2 className="text-3xl font-bold uppercase mb-3 text-center">Skills</h2>
       <div className="container-skills">
-      <div className="html">
+        <div className="html">
           <p className="bar-title">
             HTML5 / CSS3 / Bootstrap5​ / Tailwind CSS
             <span className="percent align-right">90%</span>
@@ -14,7 +14,7 @@ const Skills = () => {
           <div className="bar">
             <div className="bar-fill bar-fill-html start"></div>
           </div>
-          </div>
+        </div>
         <div className="angular">
           <p className="bar-title">
             React JS / Angular 12
@@ -24,26 +24,26 @@ const Skills = () => {
             <div className="bar-fill bar-fill-responsive start"></div>
           </div>
         </div>
-          <div className="javascript">
-            <p className="bar-title">
-              Javascript / Typescript
-              <span className="percent align-right">80%</span>
-            </p>
-            <div className="bar">
-              <div className="bar-fill bar-fill-javascript start"></div>
-            </div>
-          </div>
-        </div>
-        <div className="java">
+        <div className="javascript">
           <p className="bar-title">
-            Java / .Net Core
-            <span className="percent align-right">70%</span>
+            Javascript / Typescript
+            <span className="percent align-right">80%</span>
           </p>
           <div className="bar">
-            <div className="bar-fill bar-fill-java start"></div>
+            <div className="bar-fill bar-fill-javascript start"></div>
           </div>
         </div>
       </div>
+      <div className="java">
+        <p className="bar-title">
+          Java / .Net Core
+          <span className="percent align-right">70%</span>
+        </p>
+        <div className="bar">
+          <div className="bar-fill bar-fill-java start"></div>
+        </div>
+      </div>
+    </div>
   );
 };
 
